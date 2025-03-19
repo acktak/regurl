5876a4d6d376a1985123893b0387aa21032f9154
Jamel <jamel.ali-hadef@outscale.com>
2025-03-19 17:39:55 +0100

    fix core and add features

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from src.gui.app import TkinterApp
+from src.gui.app import WebScraperApp
 import tkinter as tk
+import logging
+import os
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG").upper()
+logging.getLogger().setLevel(LOG_LEVEL)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = TkinterApp(root)
+    app = WebScraperApp(root)
     root.mainloop()
